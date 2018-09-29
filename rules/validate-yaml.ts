619fee0c6e63c47d0b5f7c1a63ef4be97d3241cc
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-09-30 01:08:30 +0200

    h

--- rules/validate-yaml.ts
+++ rules/validate-yaml.ts
@@ -3,11 +3,11 @@ import fs from 'fs'
 
 export const validateYaml = () => {
   message(`files ${danger.git.modified_files.join(', ')}`)
-  if (!(`test.yaml` in danger.git.modified_files)) {
+  if (!(danger.git.modified_files.includes("test.yaml"))) {
     return
   }
   
-  const fileContent = fs.readFileSync(`test.yaml`).toString();
+  const fileContent = fs.readFileSync("test.yaml").toString();
   message(`test ${fileContent}`)
 };
 
