fc8d4348174790ed912c759079216145f5c54287
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-09-30 01:18:47 +0200

    p

--- rules/validate-yaml.ts
+++ rules/validate-yaml.ts
@@ -1,14 +1,18 @@
 import { danger, message } from 'danger';
 
-export const validateYaml = () => {
+export const validateYaml = async () => {
   message(`files ${danger.git.modified_files.join(', ')}`)
   if (!(danger.git.modified_files.includes("test.yaml"))) {
     return
   }
 
-  const test = danger.git.JSONDiffForFile("test.yaml")
+  const test = await danger.git.diffForFile("test.yaml")
 
   message(`test ${JSON.stringify(test)}`)
+
+  const test2 = await danger.git.structuredDiffForFile("test.yaml")
+
+  message(`test2 ${JSON.stringify(test2)}`)
 };
 
 export default async () => {
