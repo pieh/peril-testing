44ed87aec842724856a62b5536782f515ac8c341
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-09-30 01:21:47 +0200

    g

--- rules/validate-yaml.ts
+++ rules/validate-yaml.ts
@@ -6,13 +6,9 @@ export const validateYaml = async () => {
     return
   }
 
-  const test = await danger.git.diffForFile("test.yaml")
+  const test = await danger.github.utils.fileContents("test.yaml")
 
   message(`test ${JSON.stringify(test)}`)
-
-  const test2 = await danger.git.structuredDiffForFile("test.yaml")
-
-  message(`test2 ${JSON.stringify(test2)}`)
 };
 
 export default async () => {
