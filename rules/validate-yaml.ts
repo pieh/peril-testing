4149473f505e043d4dafad837332ca0993d84eb8
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-09-30 01:10:55 +0200

    hm

--- rules/validate-yaml.ts
+++ rules/validate-yaml.ts
@@ -1,5 +1,5 @@
 import { danger, message } from 'danger';
-import fs from 'fs'
+import * as fs from 'fs'
 
 export const validateYaml = () => {
   message(`files ${danger.git.modified_files.join(', ')}`)
