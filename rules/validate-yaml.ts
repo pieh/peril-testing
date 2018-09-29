ad71c45d9173646aaedc454cd1187096d3376448
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-09-30 01:37:47 +0200

    add js-yaml

--- rules/validate-yaml.ts
+++ rules/validate-yaml.ts
@@ -1,17 +1,21 @@
-import { danger, message } from 'danger';
+import { danger, message, fail } from 'danger';
+import { load as yamlLoad } from 'js-yaml'
 
 export const validateYaml = async () => {
   console.log('validating')
-  if (!(danger.git.modified_files.includes("test.yaml"))) {
-    console.log('no test.yaml in changed files')
+  const filePath = "test.yaml"
+  if (!(danger.git.modified_files.includes(filePath))) {
+    console.log(`no ${filePath} in changed files`)
     return
   }
 
-  console.log(`getting fileContent`)
-
-  const test = await danger.github.utils.fileContents("test.yaml")
-
-  console.log(`test ${JSON.stringify(test)}`)
+  const textContent = await danger.github.utils.fileContents(filePath)
+  try {
+    const content = yamlLoad(textContent)
+    message(`${filePath} content:\n${content}`)
+  } catch (e) {
+    fail(`${filePath} is not valid:\n${e.message}`)
+  }
 };
 
 export default async () => {
