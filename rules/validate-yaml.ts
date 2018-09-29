55e36c41e93804d70094c1f2a7a7e53ed77a11b8
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-09-30 01:57:18 +0200

    some validation

--- rules/validate-yaml.ts
+++ rules/validate-yaml.ts
@@ -12,6 +12,27 @@ export const validateYaml = async () => {
   const textContent = await danger.github.utils.fileContents(filePath)
   try {
     const content = yamlLoad(textContent)
+
+    if (!Array.isArray(content)) {
+      fail(`${filePath}: content need to be array`)
+      return
+    }
+
+    content.forEach((item, index) => {
+      // assume item is object (for now)
+      const { name, description } = item
+
+      if (!name) {
+        fail(`${filePath}: item #${index+1} is missing required 'name' property`)
+      } else if (typeof name !== 'string') {
+        fail(`${filePath}: item #${index+1} 'name' property need to be string`)
+      }
+
+      if (description && typeof description !== 'string') {
+        fail(`${filePath}: item #${index+1} optional 'description' property need to be string`)
+      }
+    })
+
     message(`${filePath} content:\n${content}`)
   } catch (e) {
     fail(`${filePath} is not valid:\n${e.message}`)
