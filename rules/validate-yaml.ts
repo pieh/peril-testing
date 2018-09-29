8b74eedbb436ceaa5dae34f859233fbf3bf4779a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-09-30 01:24:41 +0200

    q

--- rules/validate-yaml.ts
+++ rules/validate-yaml.ts
@@ -1,16 +1,19 @@
 import { danger, message } from 'danger';
 
 export const validateYaml = async () => {
-  message(`files ${danger.git.modified_files.join(', ')}`)
+  console.log('validating')
   if (!(danger.git.modified_files.includes("test.yaml"))) {
+    console.log('no test.yaml in changed files')
     return
   }
 
+  console.log(`getting fileContent`)
+
   const test = await danger.github.utils.fileContents("test.yaml")
 
-  message(`test ${JSON.stringify(test)}`)
+  console.log(`test ${JSON.stringify(test)}`)
 };
 
 export default async () => {
-  validateYaml()
+  return validateYaml()
 };
