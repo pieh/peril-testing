6613973014bb244b9765123d8150b6d71a0ce11a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-09-30 02:48:42 +0200

    more

--- rules/validate-yaml.ts
+++ rules/validate-yaml.ts
@@ -1,15 +1,14 @@
 import { danger, message, fail } from 'danger';
 import { load as yamlLoad } from 'js-yaml'
+import * as path from 'path'
 
 const filePath = "data/test.yaml"
+const supportedExts = ['.txt']
 
 export const validateYaml = async () => {
   console.log('validating')
 
-  const imagesDirReponse = await danger.github.api.repos.getContent({repo: 'peril-testing', owner: 'pieh', path: 'data/images/'})
-  const images = imagesDirReponse.data.map(({name}) => name)
-  console.log(images)
-  
+ 
   if (!(danger.git.modified_files.includes(filePath))) {
     console.log(`no ${filePath} in changed files`)
     return
@@ -24,9 +23,15 @@ export const validateYaml = async () => {
       return
     }
 
+    console.log('repo',  danger.github.pr.head.repo)
+
+    const imagesDirReponse = await danger.github.api.repos.getContent({repo: 'peril-testing', owner: 'pieh', path: 'data/images/', ref: danger.github.pr.head.ref})
+    const images = imagesDirReponse.data.map(({name}) => `images/${name}`)
+    console.log(images)
+
     content.forEach((item, index) => {
       // assume item is object (for now)
-      const { name, description } = item
+      const { name, description, image } = item
 
       if (!name) {
         fail(`${filePath}: item #${index+1} is missing required 'name' property`)
@@ -37,6 +42,17 @@ export const validateYaml = async () => {
       if (description && typeof description !== 'string') {
         fail(`${filePath}: item #${index+1} optional 'description' property need to be string`)
       }
+
+      if (!image) {
+        fail(`${filePath}: item #${index+1} is missing required 'image' property`)
+      } else if (typeof image !== 'string') {
+        fail(`${filePath}: item #${index+1} 'image' property need to be string`)
+      } else if (!images.includes(image)) {
+        fail(`${filePath}: item #${index+1} 'image' ${image} doesn't point to existing file`)
+      } else if (!supportedExts.includes(path.extname(image))) {
+        fail(`${filePath}: item #${index+1} 'image' ${image} use unsporrted file format - use one of following: ${supportedExts.join(', ')}`)
+      }
+
     })
   } catch (e) {
     fail(`${filePath} is not valid:\n${e.message}`)
