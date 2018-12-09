2fef05434cde49098b3d4dd9100655242adc6f7a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-12-09 14:58:44 +0100

    changed files

--- rules/handle-comment-commands.ts
+++ rules/handle-comment-commands.ts
@@ -2,6 +2,15 @@
 import { danger, warn } from 'danger';
 import * as path from 'path'
 
+type FileData = {
+  filename: string,
+  status: string,
+}
+
+type getFilesReponse = {
+  data: FileData[]
+}
+
 const getBranchInformation = (responseFragment: any) => {
   const [repo, owner] = responseFragment.repo.full_name.split('/')
   return {
@@ -20,8 +29,8 @@ const getPRInfo = async (number: Number) => {
     repo,
     number,
   })
-
-  const filesData = await danger.github.api.pullRequests.getFiles({
+  
+  const filesData: getFilesReponse = await danger.github.api.pullRequests.getFiles({
     owner: mainOwner,
     repo,
     number,
@@ -31,13 +40,13 @@ const getPRInfo = async (number: Number) => {
 
   return {
     base: getBranchInformation(prData.data.base),
-    head: getBranchInformation(prData.data.head)
+    head: getBranchInformation(prData.data.head),
+    files: filesData.data.filter(fileData => fileData.status !== `removed`).map(fileData => fileData.filename)
   }
 }
 
 export const shouldFormat = async () => {
   if (!danger.github.issue.pull_request) {
-    // this is issue, not PR
     console.log(`NOT PR`)
     return
   }
