678dc114f2b600230e1904f9cd289f8cbf046a35
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-12-09 13:54:16 +0100

    !

--- rules/handle-comment-commands.ts
+++ rules/handle-comment-commands.ts
@@ -2,7 +2,7 @@
 import { danger, warn } from 'danger';
 
 export const shouldFormat = async () => {
-  if (danger.github.issue.pull_request) {
+  if (!danger.github.issue.pull_request) {
     // this is issue, not PR
     return
   }
