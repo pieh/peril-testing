7a7662e4022143d4b21780035d4130d65cc96422
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-12-09 14:11:40 +0100

    d

--- rules/handle-comment-commands.ts
+++ rules/handle-comment-commands.ts
@@ -1,12 +1,25 @@
 
 import { danger, warn } from 'danger';
+import * as path from 'path'
 
 export const shouldFormat = async () => {
   if (!danger.github.issue.pull_request) {
     // this is issue, not PR
+    console.log(`NOT PR`)
     return
   }
-  console.log('checking if should format')
+
+  if (!danger.github.comment.body.includes(`format`)) {
+    console.log(`comment doesn't include "format"`)
+    return
+  }
+
+  // 
+  const prNumber = danger.github.issue.number
+
+  console.log(`grabing branch data for PR #${prNumber}`)
+
+  
 
 
   console.log(danger.github)
